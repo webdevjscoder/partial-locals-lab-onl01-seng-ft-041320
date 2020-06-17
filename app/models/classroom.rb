940ef70fd4ed0a701f0fae12a91ea4674cafed32
webdevjscoder <cobianjuan19@gmail.com>
2020-06-17 13:09:34 -0700

    Done.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -13,7 +13,9 @@ class Classroom < ActiveRecord::Base
   has_many :classroom_students
   has_many :students, through: :classroom_students
 
-  def oldest_student
-    students.where("birthday is not null").order("birthday asc").first
-  end
+  # def oldest_student
+  #   students.where("birthday is not null").order("birthday asc").first
+  # end
+
+
 end
